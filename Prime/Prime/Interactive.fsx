f6d146fd203df33edd5819544232ee92eeaa730c
symbolsoft <symbolsoft7@gmail.com>
2018-09-04 12:16:04 -0400

    Partial fixes for fsx file.

--- Prime/Prime/Interactive.fsx
+++ Prime/Prime/Interactive.fsx
@@ -2,15 +2,15 @@
 // Copyright (C) Bryan Edds, 2013-2018.
 
 #I __SOURCE_DIRECTORY__
-#r "../../Prime/Prime.Dependencies/System.ValueTuple.4.3.0/lib/portable-net40+sl4+win8+wp8/System.ValueTuple.dll"
-#r "../../Prime/Prime.Dependencies/FParsec/FParsecCS.dll" // MUST be referenced BEFORE FParsec.dll!
-#r "../../Prime/Prime.Dependencies/FParsec/FParsec.dll"
-#r "../../Prime/Prime.Dependencies/xunit/xunit.core.dll"
-#r "../../Prime/Prime.Dependencies/xunit/xunit.abstractions.dll"
-#r "../../Prime/Prime.Dependencies/xunit/xunit.assert.dll"
-#r "../../Prime/Prime.Dependencies/xunit/xunit.execution.desktop.dll"
-#r "../../Prime/Prime.Dependencies/FsCheck/FsCheck.dll"
-#r "../../Prime/Prime.Dependencies/FsCheck.Xunit/FsCheck.Xunit.dll"
+#r "../../packages/System.ValueTuple.4.4.0/lib/portable-net40+sl4+win8+wp8/System.ValueTuple.dll"
+#r "../../packages/FParsec.1.0.3/lib/net40-client/FParsecCS.dll" // MUST be referenced BEFORE FParsec.dll!
+#r "../../packages/FParsec.1.0.3/lib/net40-client/FParsec.dll"
+//#r "../../Prime/Prime.Dependencies/xunit/xunit.core.dll"
+//#r "../../Prime/Prime.Dependencies/xunit/xunit.abstractions.dll"
+//#r "../../Prime/Prime.Dependencies/xunit/xunit.assert.dll"
+//#r "../../Prime/Prime.Dependencies/xunit/xunit.execution.desktop.dll"
+#r "../../packages/FsCheck.2.10.10/lib/net452/FsCheck.dll"
+#r "../../packages/FsCheck.Xunit.2.10.10/lib/net452/FsCheck.Xunit.dll"
 #r "../../Prime/Prime/bin/Debug/Prime.exe"
 
 open System
