26f511b6b344271a9f43a075e03e17c4d246185e
bryanedds <bryanedds@gmail.com>
2019-07-16 14:25:55 -0400

    Release v3.6.6.

--- Prime/Prime/Binding.fs
+++ Prime/Prime/Binding.fs
@@ -4,13 +4,13 @@
 namespace Prime
 open Prime
 
-type [<NoEquality; NoComparison>] Binding<'m, 's, 'w when 's :> Participant and 'w :> EventSystem<'w>> =
+type [<NoEquality; NoComparison>] BindingValue<'a, 's, 'w when 's :> Participant and 'w :> EventSystem<'w>> =
     { Stream : Stream<obj, 'w>
-      MakeValue : Event<obj, 's> -> 'm }
+      MakeValue : Event<obj, 's> -> 'a }
 
-type [<NoEquality; NoComparison>] Binding<'m, 'e, 's, 'w when 's :> Participant and 'w :> EventSystem<'w>> =
-    | Message of Binding<'m, 's, 'w>
-    | Command of Binding<'e, 's, 'w>
+type [<NoEquality; NoComparison>] Binding<'m, 'c, 's, 'w when 's :> Participant and 'w :> EventSystem<'w>> =
+    | MessageBinding of BindingValue<'m, 's, 'w>
+    | CommandBinding of BindingValue<'c, 's, 'w>
 
 [<RequireQualifiedAccess>]
 module Binding =
@@ -25,51 +25,106 @@ module Binding =
         { Stream = Stream.generalize stream
           MakeValue = fun (_ : Event<obj, 's>) -> value }
 
-type Binding<'m, 'e, 's, 'w when 's :> Participant and 'w :> EventSystem<'w>> with
+type Binding<'m, 'c, 's, 'w when 's :> Participant and 'w :> EventSystem<'w>> with
 
-    static member (=>) (_ : Binding<'m, 'e, 's, 'w>, source : Address<'a>) =
+    static member (=>) (_ : Binding<'m, 'c, 's, 'w>, source : Address<'a>) =
         fun (message : 'm) ->
-            Message (Binding.makeSimple (Stream.make source) message)
+            MessageBinding (Binding.makeSimple (Stream.make source) message)
 
-    static member (=>) (_ : Binding<'m, 'e, 's, 'w>, source : Stream<'a, 'w>) =
+    static member (=>) (_ : Binding<'m, 'c, 's, 'w>, source : Stream<'a, 'w>) =
         fun (message : 'm) ->
-            Message (Binding.makeSimple source message)
+            MessageBinding (Binding.makeSimple source message)
 
-    static member (=|>) (_ : Binding<'m, 'e, 's, 'w>, source : Address<'a>) =
+    static member (=|>) (_ : Binding<'m, 'c, 's, 'w>, source : Address<'a>) =
         fun (message : Event<'a, 's> -> 'm) ->
-            Message (Binding.make (Stream.make source) message)
+            MessageBinding (Binding.make (Stream.make source) message)
 
-    static member (=|>) (_ : Binding<'m, 'e, 's, 'w>, source : Stream<'a, 'w>) =
+    static member (=|>) (_ : Binding<'m, 'c, 's, 'w>, source : Stream<'a, 'w>) =
         fun (message : Event<'a, 's> -> 'm) ->
-            Message (Binding.make source message)
+            MessageBinding (Binding.make source message)
 
-    static member (=>!) (_ : Binding<'m, 'e, 's, 'w>, source : Address<'a>) =
-        fun (command : 'e) ->
-            Command (Binding.makeSimple (Stream.make source) command)
+    static member (=>!) (_ : Binding<'m, 'c, 's, 'w>, source : Address<'a>) =
+        fun (command : 'c) ->
+            CommandBinding (Binding.makeSimple (Stream.make source) command)
 
-    static member (=>!) (_ : Binding<'m, 'e, 's, 'w>, source : Stream<'a, 'w>) =
-        fun (command : 'e) ->
-            Command (Binding.makeSimple source command)
+    static member (=>!) (_ : Binding<'m, 'c, 's, 'w>, source : Stream<'a, 'w>) =
+        fun (command : 'c) ->
+            CommandBinding (Binding.makeSimple source command)
 
-    static member (=|>!) (_ : Binding<'m, 'e, 's, 'w>, source : Address<'a>) =
-        fun (command : Event<'a, 's> -> 'e) ->
-            Command (Binding.make (Stream.make source) command)
+    static member (=|>!) (_ : Binding<'m, 'c, 's, 'w>, source : Address<'a>) =
+        fun (command : Event<'a, 's> -> 'c) ->
+            CommandBinding (Binding.make (Stream.make source) command)
 
-    static member (=|>!) (_ : Binding<'m, 'e, 's, 'w>, source : Stream<'a, 'w>) =
-        fun (command : Event<'a, 's> -> 'e) ->
-            Command (Binding.make source command)
+    static member (=|>!) (_ : Binding<'m, 'c, 's, 'w>, source : Stream<'a, 'w>) =
+        fun (command : Event<'a, 's> -> 'c) ->
+            CommandBinding (Binding.make source command)
 
 [<AutoOpen>]
 module BindingOperators =
 
-    let inline (=>) source message : Binding<'m, 'e, 's, 'w> =
-        (Unchecked.defaultof<Binding<'m, 'e, 's, 'w>> => source) message
+    let inline (=>) source message : Binding<'m, 'c, 's, 'w> =
+        (Unchecked.defaultof<Binding<'m, 'c, 's, 'w>> => source) message
 
-    let inline (=|>) source message : Binding<'m, 'e, 's, 'w> =
-        (Unchecked.defaultof<Binding<'m, 'e, 's, 'w>> =|> source) message
+    let inline (=|>) source message : Binding<'m, 'c, 's, 'w> =
+        (Unchecked.defaultof<Binding<'m, 'c, 's, 'w>> =|> source) message
 
-    let inline (=>!) source message : Binding<'m, 'e, 's, 'w> =
-        (Unchecked.defaultof<Binding<'m, 'e, 's, 'w>> =>! source) message
+    let inline (=>!) source message : Binding<'m, 'c, 's, 'w> =
+        (Unchecked.defaultof<Binding<'m, 'c, 's, 'w>> =>! source) message
 
-    let inline (=|>!) source message : Binding<'m, 'e, 's, 'w> =
-        (Unchecked.defaultof<Binding<'m, 'e, 's, 'w>> =|>! source) message
+    let inline (=|>!) source message : Binding<'m, 'c, 's, 'w> =
+        (Unchecked.defaultof<Binding<'m, 'c, 's, 'w>> =|>! source) message
+
+/// A model-message-command signal.
+type Signal<'message, 'command> =
+    | Message of 'message
+    | Command of 'command
+    | Signals of Signal<'message, 'command> list
+
+[<RequireQualifiedAccess>]
+module Signal =
+
+    let message message = Message message
+    let command command = Command command
+    let many nexts = Signals nexts
+    let empty = Signals []
+
+    let rec private processModelInternal<'model, 'message, 'command, 'p, 'w when 'p :> Participant>
+        processMessage
+        processCommand
+        (model : Lens<'model, 'w>)
+        (participant : 'p)
+        (next : Signal<'message, 'command>)
+        (world : 'w) =
+        match next with
+        | Message message ->
+            let (modelValue, next) = processMessage (message, model.Get world, participant, world)
+            let world = model.Set modelValue world
+            processModelInternal processMessage processCommand model participant next world
+        | Command command ->
+            let (world, next) = processCommand (command, model.Get world, participant, world)
+            processModelInternal processMessage processCommand model participant next world
+        | Signals nexts ->
+            List.fold (flip (processModelInternal processMessage processCommand model participant)) world nexts
+
+    let processModel processMessage processCommand model participant bindings world =
+        List.fold (fun world binding ->
+            match binding with
+            | MessageBinding binding ->
+                Stream.monitor (fun evt world ->
+                    let next = message (binding.MakeValue evt)
+                    processModelInternal processMessage processCommand model participant next world)
+                    participant binding.Stream world
+            | CommandBinding binding ->
+                Stream.monitor (fun evt world ->
+                    let next = command (binding.MakeValue evt)
+                    processModelInternal processMessage processCommand model participant next world)
+                    participant binding.Stream world)
+            world bindings
+
+[<AutoOpen>]
+module SignalOperators =
+
+    let withMessage value message = (value, Message message)
+    let withCommand value command = (value, Command command)
+    let withSignals value command = (value, Signals command)
+    let just value = (value, Signal.empty)
