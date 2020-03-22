0dec36685a4e8ea928c1cfc6c5118d1f51e5e10d
bryanedds <bryanedds@gmail.com>
2020-03-22 19:25:22 -0400

    Got test discovery working again.

--- Prime/Prime.Tests/Library.fs
+++
@@ -1,5 +0,0 @@
-namespace Prime.Tests
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
--- Prime/Prime/Interactive.fsx
+++ Prime/Prime/Interactive.fsx
@@ -4,7 +4,7 @@
 #I __SOURCE_DIRECTORY__
 #r "../../packages/FParsec.1.0.3/lib/net40-client/FParsecCS.dll" // MUST be referenced BEFORE FParsec.dll!
 #r "../../packages/FParsec.1.0.3/lib/net40-client/FParsec.dll"
-#r "../../Prime/Prime/bin/Debug/Prime.exe"
+#r "../../Prime/Prime/bin/Debug/Prime.dll"
 
 open System
 open FParsec
