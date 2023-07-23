f0ef853d719cb9a8f4caf303abb6110d7fac3f9b
bryanedds <bryanedds@gmail.com>
2023-07-23 17:10:45 -0400

    Using nuget references in fsx.

--- Prime/Prime/Interactive.fsx
+++ Prime/Prime/Interactive.fsx
@@ -2,9 +2,8 @@
 // Copyright (C) Bryan Edds, 2013-2023.
 
 #I __SOURCE_DIRECTORY__
-#r "../../packages/Csv.1.0.58/lib/net40/Csv.dll"
-#r "../../packages/FParsec.1.0.3/lib/net40-client/FParsecCS.dll" // MUST be referenced BEFORE FParsec.dll!
-#r "../../packages/FParsec.1.0.3/lib/net40-client/FParsec.dll"
+#r "nuget: Csv, 1.0.58"
+#r "nuget: FParsec, 1.0.3"
 #r "../../Prime/Prime/bin/Debug/netstandard2.0/Prime.dll"
 
 open System
