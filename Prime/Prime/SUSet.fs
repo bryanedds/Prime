c982c74a8c5b724d4a6d57d5e3ea48628e9f178d
bryanedds <bryanedds@gmail.com>
2023-07-25 15:15:45 -0400

    More doc comments.

--- Prime/Prime/SUSet.fs
+++ Prime/Prime/SUSet.fs
@@ -6,11 +6,10 @@ open System
 open System.Collections
 open System.Collections.Generic
 
-// TODO: document this!
-
 [<RequireQualifiedAccess>]
 module SUSet =
 
+    /// A hashing set that supports transaction-based rewinding with a more convenient interface than STSet.
     type [<ReferenceEquality>] SUSet<'a when 'a : equality> =
         private
             { mutable Set : 'a STSet }
@@ -25,141 +24,170 @@ module SUSet =
             member this.GetEnumerator () =
                 (this :> 'a seq).GetEnumerator () :> IEnumerator
 
+    /// Create a SUSet containing the given sequence of values.
     let makeFromSeq<'a when 'a : equality> comparer config items =
         { Set = STSet.makeFromSeq<'a> comparer config items }
 
+    /// Create an empty SUSet.
     let makeEmpty<'a when 'a : equality> comparer config =
         { Set = STSet.makeEmpty<'a> comparer config }
 
+    /// Get the comparer function used to determine uniqueness in a SUSet.
     let getComparer set =
         let struct (result, tset) = STSet.getComparer set.Set
         set.Set <- tset
         result
 
+    /// Get the semantic configuration of the SUSet.
     let getConfig set =
         let struct (result, tset) = STSet.getConfig set.Set
         set.Set <- tset
         result
 
+    /// Add an element to a SUSet.
     let add value set =
         { Set = STSet.add value set.Set }
 
+    /// Remove all matching elements from a SUSet.
     let remove value set =
         { Set = STSet.remove value set.Set }
 
+    /// Remove all elements from a SUSet.
     let clear set =
         { Set = STSet.clear set.Set }
 
-    /// Add all the given values to the set.
-    let addMany values set =
-        { Set = STSet.addMany values set.Set }
-
-    /// Remove all the given values from the set.
-    let removeMany values set =
-        { Set = STSet.removeMany values set.Set }
-
-    let length set =
-        let struct (result, tset) = STSet.length set.Set
-        set.Set <- tset
-        result
-
+    /// Check that a SUSet has no elements.
     let isEmpty set =
         let struct (result, tset) = STSet.isEmpty set.Set
         set.Set <- tset
         result
 
+    /// Check that a SUSet has one or more elements.
     let notEmpty set =
         not (isEmpty set)
 
+    /// Get the length of a SUSet (constant-time).
+    let length set =
+        let struct (result, tset) = STSet.length set.Set
+        set.Set <- tset
+        result
+
+    /// Determine that a SUSet contains the given value.
     let contains value set =
         let struct (result, tset) = STSet.contains value set.Set
         set.Set <- tset
         result
 
-    let toSeq (set : _ SUSet) =
-        set :> _ seq
+    /// Add all the given values to a SUSet.
+    let addMany values set =
+        { Set = STSet.addMany values set.Set }
 
-    let toHashSet (set : _ SUSet) =
-        let struct (hashSet, tset) = STSet.toHashSet set.Set
-        set.Set <- tset
-        hashSet
-
+    /// Remove all the given values from a SUSet.
+    let removeMany values set =
+        { Set = STSet.removeMany values set.Set }
+        
+    /// Make a SUSet from a sequence of values.
     let ofSeq comparer config values =
         Seq.fold
             (fun map value -> add value map)
             (makeEmpty comparer config)
             values
 
+    /// Convert a SUSet to a seq. Note that entire set is iterated eagerly since the underlying HashMap could
+    /// otherwise opaquely change during iteration.
+    let toSeq (set : _ SUSet) =
+        set :> _ seq
+
+    /// Convert a SUSet to a HashSet.
+    let toHashSet (set : _ SUSet) =
+        let struct (hashSet, tset) = STSet.toHashSet set.Set
+        set.Set <- tset
+        hashSet
+
+    /// Fold over the elements of a SUSet.
     let fold folder state set =
         let struct (result, tset) = STSet.fold folder state set.Set
         set.Set <- tset
         result
 
+    /// Map the elements of a SUSet.
     let map mapper set =
         let struct (result, tset) = STSet.map mapper set.Set
         set.Set <- tset
         { Set = result }
 
+    /// Filter the elements of a SUSet.
     let filter pred set =
         let struct (result, tset) = STSet.filter pred set.Set
         set.Set <- tset
         { Set = result }
 
+    /// Determine equality of two SUSets.
     let equals set set2 =
         let struct (result, tset, tset2) = STSet.equals set.Set set2.Set
         set.Set <- tset
         set2.Set <- tset2
         result
 
+    /// Construct a union HashSet.
     let unionFast set set2 =
         let struct (result, tset, tset2) = STSet.unionFast set.Set set2.Set
         set.Set <- tset
         set2.Set <- tset2
         result
 
+    /// Construct an intersection HashSet.
     let intersectFast set set2 =
         let struct (result, tset, tset2) = STSet.intersectFast set.Set set2.Set
         set.Set <- tset
         set2.Set <- tset2
         result
 
+    /// Construct a disjoint HashSet.
     let disjointFast set set2 =
         let struct (result, tset, tset2) = STSet.disjointFast set.Set set2.Set
         set.Set <- tset
         set2.Set <- tset2
         result
 
+    /// Construct a difference HashSet.
     let differenceFast set set2 =
         let struct (result, tset, tset2) = STSet.differenceFast set.Set set2.Set
         set.Set <- tset
         set2.Set <- tset2
         result
 
+    /// Construct a union SUSet.
     let union config set set2 =
         let struct (result, tset, tset2) = STSet.union config set.Set set2.Set
         set.Set <- tset
         set2.Set <- tset2
         { Set = result }
 
+    /// Construct an intersection SUSet.
     let intersect config set set2 =
         let struct (result, tset, tset2) = STSet.intersect config set.Set set2.Set
         set.Set <- tset
         set2.Set <- tset2
         { Set = result }
 
+    /// Construct a disjoint SUSet.
     let disjoint config set set2 =
         let struct (result, tset, tset2) = STSet.disjoint config set.Set set2.Set
         set.Set <- tset
         set2.Set <- tset2
         { Set = result }
 
+    /// Construct a difference SUSet.
     let difference config set set2 =
         let struct (result, tset, tset2) = STSet.difference config set.Set set2.Set
         set.Set <- tset
         set2.Set <- tset2
         { Set = result }
 
+    /// Make a SUSet with a single element.
     let singleton<'a when 'a : equality> comparer config item =
         { Set = STSet.singleton<'a> comparer config item }
 
+/// A hashing set that supports transaction-based rewinding with a more convenient interface than STSet.
 type SUSet<'a when 'a : equality> = 'a SUSet.SUSet
