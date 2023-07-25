327f010b4cf68c332c45f7bdf6c89d1ecc658268
bryanedds <bryanedds@gmail.com>
2023-07-25 15:08:01 -0400

    More doc comments.

--- Prime/Prime/STSet.fs
+++ Prime/Prime/STSet.fs
@@ -5,8 +5,6 @@ namespace Prime
 open System
 open System.Collections.Generic
 
-// TODO: document this!
-
 [<RequireQualifiedAccess>]
 module STSet =
 
@@ -15,6 +13,7 @@ module STSet =
         | Remove of remove : 'a
         | Clear
 
+    /// A segmented hashing set that supports transaction-based rewinding.
     type [<ReferenceEquality>] STSet<'a when 'a : equality> =
         private
             { mutable STSetOpt : 'a STSet
@@ -78,6 +77,7 @@ module STSet =
         then validate2 set
         else set
 
+    /// Create an STSet containing the element of the given hash.
     let makeFromSegmentedHashSet (comparer : 'a IEqualityComparer) config (hashSet : 'a SHashSet) =
         if TConfig.isFunctional config then
             let set =
@@ -97,19 +97,24 @@ module STSet =
               Logs = []
               LogsLength = 0 }
 
+    /// Create a STSet containing the given sequence of values.
     let makeFromSeq<'a when 'a : equality> comparer config (items : 'a seq) =
         let hashSet = SHashSet.ofSeq comparer items
         makeFromSegmentedHashSet comparer config hashSet
 
+    /// Create an empty STSet.
     let makeEmpty<'a when 'a : equality> comparer config =
         makeFromSeq<'a> comparer config Seq.empty
 
+    /// Get the comparer function used to determine uniqueness in a STSet.
     let getComparer set =
         struct (set.HashSet.Comparer, set)
 
+    /// Get the semantic configuration of the STSet.
     let getConfig set =
         struct (set.TConfig, set)
 
+    /// Add an element to a STSet.
     let add value set =
         if TConfig.isFunctional set.TConfig then
             update (fun set ->
@@ -119,6 +124,7 @@ module STSet =
                 set
         else set.HashSet.Add value |> ignore; set
 
+    /// Remove all matching elements from a STSet.
     let remove value set =
         if TConfig.isFunctional set.TConfig then
             update (fun set ->
@@ -128,6 +134,7 @@ module STSet =
                 set
         else set.HashSet.Remove value |> ignore; set
 
+    /// Remove all elements from a STSet.
     let clear set =
         if TConfig.isFunctional set.TConfig then
             update (fun set ->
@@ -137,29 +144,39 @@ module STSet =
                 set
         else set.HashSet.Clear (); set
 
+    /// Check that a STSet has no elements.
     let isEmpty set =
         let set = validate set
         struct (set.HashSet.Count = 0, set)
 
+    /// Check that a STSet has one or more elements.
     let notEmpty set =
         mapFst' not (isEmpty set)
 
-    /// Get the length of the set (constant-time).
+    /// Get the length of a STSet (constant-time).
     let length set =
         let set = validate set
         struct (set.HashSet.Count, set)
 
+    /// Determine that a STSet contains the given value.
     let contains value set =
         let set = validate set
         struct (set.HashSet.Contains value, set)
 
-    /// Add all the given values to the set.
+    /// Add all the given values to a STSet.
     let addMany values set =
         Seq.fold (flip add) set values
 
-    /// Remove all the given values from the set.
+    /// Remove all the given values from a STSet.
     let removeMany values set =
         Seq.fold (flip remove) set values
+        
+    /// Make a STSet from a sequence of values.
+    let ofSeq comparer config values =
+        Seq.fold
+            (fun map value -> add value map)
+            (makeEmpty comparer config)
+            values
 
     /// Convert a STSet to a seq. Note that entire set is iterated eagerly since the underlying HashMap could
     /// otherwise opaquely change during iteration.
@@ -174,72 +191,88 @@ module STSet =
         let result = SHashSet.makeFromSegmentedHashSet set.HashSet
         struct (result, set)
 
+    /// Fold over the elements of a STSet.
     let fold folder state set =
         let struct (seq, set) = toSeq set
         let result = Seq.fold folder state seq
         struct (result, set)
 
+    /// Map the elements of a STSet.
     let map mapper set =
         fold
             (fun set value -> add (mapper value) set)
             (makeEmpty set.HashSet.Comparer set.TConfig)
             set
 
+    /// Filter the elements of a STSet.
     let filter pred set =
         fold
             (fun set value -> if pred value then add value set else set)
             (makeEmpty set.HashSet.Comparer set.TConfig)
             set
 
+    /// Determine equality of two STSets.
     let equals set set2 =
         let (set, set2) = (validate set, validate set2)
         struct (set.HashSet.SetEquals set2.HashSet, set, set2)
 
+    /// Construct a union HashSet.
     let unionFast set set2 =
         let (set, set2) = (validate set, validate set2)
         let result = SHashSet.makeFromSegmentedHashSet set.HashSet
         result.UnionWith set2.HashSet
         struct (result, set, set2)
 
+    /// Construct an intersection HashSet.
     let intersectFast set set2 =
         let (set, set2) = (validate set, validate set2)
         let result = SHashSet.makeFromSegmentedHashSet set.HashSet
         result.IntersectWith set2.HashSet
         struct (result, set, set2)
 
+    /// Construct a disjoint HashSet.
     let disjointFast set set2 =
         let (set, set2) = (validate set, validate set2)
         let result = SHashSet.makeFromSegmentedHashSet set.HashSet
         result.SymmetricExceptWith set2.HashSet
         struct (result, set, set2)
 
+    /// Construct a difference HashSet.
     let differenceFast set set2 =
         let (set, set2) = (validate set, validate set2)
         let result = SHashSet.makeFromSegmentedHashSet set.HashSet
         result.ExceptWith set2.HashSet
         struct (result, set, set2)
 
+    /// Construct a union STSet.
     let union config set set2 =
         let struct (result, set, set2) = unionFast set set2
         struct (makeFromSegmentedHashSet set.HashSet.Comparer config result, set, set2)
 
+    /// Construct an intersection STSet.
     let intersect config set set2 =
         let struct (result, set, set2) = intersectFast set set2
         struct (makeFromSegmentedHashSet set.HashSet.Comparer config result, set, set2)
 
+    /// Construct a disjoint STSet.
     let disjoint config set set2 =
         let struct (result, set, set2) = disjointFast set set2
         struct (makeFromSegmentedHashSet set.HashSet.Comparer config result, set, set2)
 
+    /// Construct a difference STSet.
     let difference config set set2 =
         let struct (result, set, set2) = differenceFast set set2
         struct (makeFromSegmentedHashSet set.HashSet.Comparer config result, set, set2)
 
+    /// Make a STSet with a single element.
     let singleton<'a when 'a : equality> comparer config (item : 'a) =
         makeFromSeq comparer config [item]
 
+/// A hashing set that supports transaction-based rewinding.
 type STSet<'a when 'a : equality> = 'a STSet.STSet
 
 [<AutoOpen>]
 module STSetBuilder =
+
+    /// Build a STSet.
     let tset<'a when 'a : equality> = TExprBuilder<'a STSet> ()
