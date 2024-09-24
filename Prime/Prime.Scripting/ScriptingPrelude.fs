713fc343a2969e9b8d2fc3b451118b4672d2eadd
bryanedds <bryanedds@gmail.com>
2024-09-23 21:50:21 -0400

    Move _ToCodeString fns to scripting project.

---
+++ Prime/Prime.Scripting/ScriptingPrelude.fs
@@ -0,0 +1,46 @@
+﻿// Prime - A PRIMitivEs code library.
+// Copyright (C) Bryan Edds, 2013-2023.
+
+namespace Prime
+open System
+
+[<RequireQualifiedAccess>]
+module String =
+
+    /// Convert a bool to a string that works well in code.
+    let boolToCodeString (bool : bool) =
+        if bool then "true" else "false"
+
+    /// Convert an int64 to a string that works well in code.
+    let int64ToCodeString (num : int64) =
+        let numStr = string num
+        numStr + "L"
+
+    /// Convert a single to a string that works well in code.
+    let singleToCodeString (num : single) =
+        if not (Single.IsNaN num) then
+            let decimaled = num.ToString ("N7")
+            let cleaned = decimaled.TrimEnd('0').Replace(",","")
+            let zeroed = if cleaned.EndsWith "." then cleaned + "0" else cleaned
+            zeroed + "f"
+        else string num
+
+    /// Convert a double to a string that works well in code.
+    let doubleToCodeString (num : double) =
+        if not (Double.IsNaN num) then
+            let decimaled = num.ToString ("N15")
+            let cleaned = decimaled.TrimEnd('0').Replace(",","")
+            if cleaned.EndsWith "." then cleaned + "0" else cleaned
+        else string num
+
+    /// Convert a number to a string that works well in code.
+    let numberToCodeString (num : obj) =
+        match num with
+        | :? bool as bool -> boolToCodeString bool
+        | :? char as char -> string char
+        | :? int as int -> string int
+        | :? int64 as int64 -> int64ToCodeString int64
+        | :? single as single -> singleToCodeString single
+        | :? double as double -> doubleToCodeString double
+        | _ -> failwithumf ()
+
--- Prime/Prime/String.fs
+++ Prime/Prime/String.fs
@@ -57,43 +57,6 @@ module String =
     let isGuid (str : string) =
         fst (Guid.TryParse str)
 
-    /// Convert a bool to a string that works well in code.
-    let boolToCodeString (bool : bool) =
-        if bool then "true" else "false"
-
-    /// Convert an int64 to a string that works well in code.
-    let int64ToCodeString (num : int64) =
-        let numStr = string num
-        numStr + "L"
-
-    /// Convert a single to a string that works well in code.
-    let singleToCodeString (num : single) =
-        if not (Single.IsNaN num) then
-            let decimaled = num.ToString ("N7")
-            let cleaned = decimaled.TrimEnd('0').Replace(",","")
-            let zeroed = if cleaned.EndsWith "." then cleaned + "0" else cleaned
-            zeroed + "f"
-        else string num
-
-    /// Convert a double to a string that works well in code.
-    let doubleToCodeString (num : double) =
-        if not (Double.IsNaN num) then
-            let decimaled = num.ToString ("N15")
-            let cleaned = decimaled.TrimEnd('0').Replace(",","")
-            if cleaned.EndsWith "." then cleaned + "0" else cleaned
-        else string num
-
-    /// Convert a number to a string that works well in code.
-    let numberToCodeString (num : obj) =
-        match num with
-        | :? bool as bool -> boolToCodeString bool
-        | :? char as char -> string char
-        | :? int as int -> string int
-        | :? int64 as int64 -> int64ToCodeString int64
-        | :? single as single -> singleToCodeString single
-        | :? double as double -> doubleToCodeString double
-        | _ -> failwithumf ()
-
     /// Converts a string into a list of characters.
     let explode (str : string) =
         let rec loop n acc =
